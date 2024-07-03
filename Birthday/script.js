6b1a5eb3b34670b517747da410d98b575f7afe0b
cryptictycoon <156873276+cryptictycoon@users.noreply.github.com>
2024-07-03 21:29:58 +0300

    Add files via upload

---
+++ Birthday/script.js
@@ -0,0 +1,8 @@
+// alert("Who are you")
+const audio = document.querySelector('audio');
+  const body = document.body;
+audio.addEventListener('play',() => {body.classList.add('play-music');
+});
+audio.addEventListener( 'pause', () =>{
+    body.classList.remove('play-music');
+});
